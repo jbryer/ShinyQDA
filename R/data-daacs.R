46b8aef23e81936f3ee7b179671f1a49f295aeff
jbryer <jason@bryer.org>
2023-03-02 20:32:27 -0500

    Added DAACS data.

---
+++ R/data-daacs.R
@@ -0,0 +1,39 @@
+#' Sample data from the Diagnostic Assessment and Achievement of College Skills (DAACS)
+#'
+#' @section About DAACS:
+#'
+#' This is a small subset of essays, essay scores, and self-regulated learning
+#' results from the Diagnostic Assessment and Achievement of College Skills (DAACS).
+#' DAACS is a suite of technological and social supports to optimize student
+#' learning. DAACS provides personalized feedback about students' strengths and
+#' weaknesses in terms of key academic and self-regulated learning skills,
+#' linking them to the resources to help them be successful students.
+#'
+#' These data are a subset of essays used in ChingYen et al (2023) College
+#' Students' Responses to Automated Feedback on Self-Regulated Learning.
+#'
+#' @section `daacs_data` Structure:
+#'
+#' ```{r, child = "man/rmd/daacs_data.Rmd"}
+#' ```
+#'
+#' @section `daacs_coding` Structure:
+#'
+#' ```{r, child = "man/rmd/daacs_codings.Rmd"}
+#' ```
+#'
+#' @section `daacs_text_responses` Structure:
+#'
+#' ```{r, child = "man/rmd/daacs_text_responses.Rmd"}
+#' ```
+#'
+#' @docType data
+#' @name daacs
+#' @format Data frames.
+#' @seealso daacs_codings daacs_text_responses daacs_rubric
+#' @source <https://daacs.net>
+#' @source <https://docs.daacs.net/overview/assessments/writing/>
+"daacs_data"
+"daacs_codings"
+"daacs_text_responses"
+"daacs_rubric"
